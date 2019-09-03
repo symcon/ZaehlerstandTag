9b2882a8b32200cd6bfd98b9098bd3fbdbd34c11
Till Brede <tb@symcon.de>
2019-09-03 09:53:55 +0200

    applied cs fixer style

--- ZaehlerstandTag/module.php
+++ ZaehlerstandTag/module.php
@@ -1,17 +1,17 @@
-<?
+<?php
 
-	class ZaehlerstandTag extends IPSModule
-	{
-		
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-			
-			$this->RegisterPropertyInteger("SourceVariable", 0);
-			$this->RegisterPropertyInteger("ValueType", 0);
-			
-		}
+declare(strict_types=1);
+
+    class ZaehlerstandTag extends IPSModule
+    {
+        public function Create()
+        {
+            //Never delete this line!
+            parent::Create();
+
+            $this->RegisterPropertyInteger('SourceVariable', 0);
+            $this->RegisterPropertyInteger('ValueType', 0);
+        }
 
         public function ApplyChanges()
         {
@@ -20,45 +20,42 @@
             parent::ApplyChanges();
 
             //Create variables
-            $this->RegisterVariableInteger("Date", "Datum", "~UnixTimestampDate", 1);
-            $this->EnableAction("Date");
+            $this->RegisterVariableInteger('Date', 'Datum', '~UnixTimestampDate', 1);
+            $this->EnableAction('Date');
 
-            if (GetValue($this->GetIDForIdent("Date")) == 0) {
-                SetValue($this->GetIDForIdent("Date"), time());
+            if (GetValue($this->GetIDForIdent('Date')) == 0) {
+                SetValue($this->GetIDForIdent('Date'), time());
             }
-            
-            $sourceVariable = $this->ReadPropertyInteger("SourceVariable");
-            if($sourceVariable > 0 && IPS_VariableExists($sourceVariable)) {
 
+            $sourceVariable = $this->ReadPropertyInteger('SourceVariable');
+            if ($sourceVariable > 0 && IPS_VariableExists($sourceVariable)) {
                 $v = IPS_GetVariable($sourceVariable);
 
-                $sourceProfile = "";
+                $sourceProfile = '';
                 if (IPS_VariableExists($sourceVariable)) {
                     $sourceProfile = $v['VariableCustomProfile'];
-                    if ($sourceProfile == "") {
+                    if ($sourceProfile == '') {
                         $sourceProfile = $v['VariableProfile'];
                     }
                 }
 
                 switch ($v['VariableType']) {
                     case 1: /* Integer */
-                        $this->RegisterVariableInteger("Reading", "Zählerstand", $sourceProfile, 3);
+                        $this->RegisterVariableInteger('Reading', 'Zählerstand', $sourceProfile, 3);
                         break;
                     case 2: /* Float */
-                        $this->RegisterVariableFloat("Reading", "Zählerstand", $sourceProfile, 3);
+                        $this->RegisterVariableFloat('Reading', 'Zählerstand', $sourceProfile, 3);
                         break;
                     default:
                         return;
                 }
-
             }
-
         }
 
-        public function RequestAction($Ident, $Value) {
-
-            switch($Ident) {
-                case "Date":
+        public function RequestAction($Ident, $Value)
+        {
+            switch ($Ident) {
+                case 'Date':
                     //Neuen Wert in die Statusvariable schreiben
                     SetValue($this->GetIDForIdent($Ident), $Value);
 
@@ -66,9 +63,8 @@
                     $this->Calculate();
                     break;
                 default:
-                    throw new Exception("Invalid Ident");
+                    throw new Exception('Invalid Ident');
             }
-
         }
 
         /**
@@ -80,39 +76,34 @@
          */
         public function Calculate()
         {
-
-            $acID = IPS_GetInstanceListByModuleID("{43192F0B-135B-4CE7-A0A7-1475603F3060}")[0];
-            $variableID = $this->ReadPropertyInteger("SourceVariable");
-            $date = GetValue($this->GetIDForIdent("Date"));
+            $acID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
+            $variableID = $this->ReadPropertyInteger('SourceVariable');
+            $date = GetValue($this->GetIDForIdent('Date'));
 
             //Falls der "erste Wert" am Tag gesucht wird, betrachte erstmal nur den gewählten Tag.
             //Falls dort keine Werte vorhanden sind sind nutzen wir die Funktionsweise von "letzter Wert" am Tag, der dann auch den Wert von Vortagen ausgibt.
-            if($this->ReadPropertyInteger("ValueType") == 0) {
+            if ($this->ReadPropertyInteger('ValueType') == 0) {
                 $values = AC_GetLoggedValues($acID, $variableID, $date, $date + (24 * 3600) - 1, 0);
-                if($values === false) {
-                    return;
-                }
-            }
-            
-            //Der letzte Wert am Tag fragt alle Werte bis zum Endzeitpunkt ab mit Limit 1.
-            //Da AC_GetLoggedValues immer den neusten Wert zuerst ausgibt ist es genau der Wert den wir suchen
-            if($this->ReadPropertyInteger("ValueType") == 1 || (isset($values) && sizeof($values) == 0)) {
-                $values = AC_GetLoggedValues($acID, $variableID, 0, $date + (24 * 3600) - 1, 1);
-                if($values === false) {
+                if ($values === false) {
                     return;
                 }
             }
 
-            if(!isset($values) || sizeof($values) == 0) {
-            	echo "Leider wurden für den gewählten Zeitraum keine Werte im Archiv gespeichert!";
-            	return;
+            //Der letzte Wert am Tag fragt alle Werte bis zum Endzeitpunkt ab mit Limit 1.
+            //Da AC_GetLoggedValues immer den neusten Wert zuerst ausgibt ist es genau der Wert den wir suchen
+            if ($this->ReadPropertyInteger('ValueType') == 1 || (isset($values) && count($values) == 0)) {
+                $values = AC_GetLoggedValues($acID, $variableID, 0, $date + (24 * 3600) - 1, 1);
+                if ($values === false) {
+                    return;
+                }
+            }
+
+            if (!isset($values) || count($values) == 0) {
+                echo 'Leider wurden für den gewählten Zeitraum keine Werte im Archiv gespeichert!';
+                return;
             }
 
             //Immer den letzten Wert im Array ausgeben
-            SetValue($this->GetIDForIdent("Reading"), $values[sizeof($values)-1]['Value']);
-
+            SetValue($this->GetIDForIdent('Reading'), $values[count($values) - 1]['Value']);
         }
-	
-	}
-
-?>
+    }
