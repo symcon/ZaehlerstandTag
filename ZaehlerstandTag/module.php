fa2770b29653d48cfbcf30929d60292b756e2192
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2023-11-16 10:12:31 +0100

    Update Style

--- ZaehlerstandTag/module.php
+++ ZaehlerstandTag/module.php
@@ -2,116 +2,116 @@
 
 declare(strict_types=1);
 
-    class ZaehlerstandTag extends IPSModule
+class ZaehlerstandTag extends IPSModule
+{
+    public function Create()
     {
-        public function Create()
-        {
-            //Never delete this line!
-            parent::Create();
+        //Never delete this line!
+        parent::Create();
 
-            $this->RegisterPropertyInteger('SourceVariable', 0);
-            $this->RegisterPropertyInteger('ValueType', 0);
+        $this->RegisterPropertyInteger('SourceVariable', 0);
+        $this->RegisterPropertyInteger('ValueType', 0);
+    }
+
+    public function ApplyChanges()
+    {
+
+        //Never delete this line!
+        parent::ApplyChanges();
+
+        //Create variables
+        $this->RegisterVariableInteger('Date', 'Datum', '~UnixTimestampDate', 1);
+        $this->EnableAction('Date');
+
+        if (GetValue($this->GetIDForIdent('Date')) == 0) {
+            SetValue($this->GetIDForIdent('Date'), time());
         }
 
-        public function ApplyChanges()
-        {
+        $sourceVariable = $this->ReadPropertyInteger('SourceVariable');
+        if ($sourceVariable > 0 && IPS_VariableExists($sourceVariable)) {
+            $v = IPS_GetVariable($sourceVariable);
 
-            //Never delete this line!
-            parent::ApplyChanges();
-
-            //Create variables
-            $this->RegisterVariableInteger('Date', 'Datum', '~UnixTimestampDate', 1);
-            $this->EnableAction('Date');
-
-            if (GetValue($this->GetIDForIdent('Date')) == 0) {
-                SetValue($this->GetIDForIdent('Date'), time());
-            }
-
-            $sourceVariable = $this->ReadPropertyInteger('SourceVariable');
-            if ($sourceVariable > 0 && IPS_VariableExists($sourceVariable)) {
-                $v = IPS_GetVariable($sourceVariable);
-
-                $sourceProfile = '';
-                if (IPS_VariableExists($sourceVariable)) {
-                    $sourceProfile = $v['VariableCustomProfile'];
-                    if ($sourceProfile == '') {
-                        $sourceProfile = $v['VariableProfile'];
-                    }
-                }
-
-                switch ($v['VariableType']) {
-                    case 1: /* Integer */
-                        $this->RegisterVariableInteger('Reading', 'Zählerstand', $sourceProfile, 3);
-                        break;
-                    case 2: /* Float */
-                        $this->RegisterVariableFloat('Reading', 'Zählerstand', $sourceProfile, 3);
-                        break;
-                    default:
-                        return;
-                }
-            }
-
-            //Add references
-            foreach ($this->GetReferenceList() as $referenceID) {
-                $this->UnregisterReference($referenceID);
-            }
+            $sourceProfile = '';
             if (IPS_VariableExists($sourceVariable)) {
-                $this->RegisterReference($sourceVariable);
+                $sourceProfile = $v['VariableCustomProfile'];
+                if ($sourceProfile == '') {
+                    $sourceProfile = $v['VariableProfile'];
+                }
             }
-        }
 
-        public function RequestAction($Ident, $Value)
-        {
-            switch ($Ident) {
-                case 'Date':
-                    //Neuen Wert in die Statusvariable schreiben
-                    SetValue($this->GetIDForIdent($Ident), $Value);
-
-                    //Berechnen
-                    $this->Calculate();
+            switch ($v['VariableType']) {
+                case 1: /* Integer */
+                    $this->RegisterVariableInteger('Reading', 'Zählerstand', $sourceProfile, 3);
+                    break;
+                case 2: /* Float */
+                    $this->RegisterVariableFloat('Reading', 'Zählerstand', $sourceProfile, 3);
                     break;
                 default:
-                    throw new Exception('Invalid Ident');
+                    return;
             }
         }
 
-        /**
-         * This function will be available automatically after the module is imported with the module control.
-         * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
-         *
-         * ZST_Calculate($id);
-         *
-         */
-        public function Calculate()
-        {
-            $acID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
-            $variableID = $this->ReadPropertyInteger('SourceVariable');
-            $date = strtotime(date('d-m-Y', GetValue($this->GetIDForIdent('Date'))) . ' 00:00:00');
-
-            //Falls der "erste Wert" am Tag gesucht wird, betrachte erstmal nur den gewählten Tag.
-            //Falls dort keine Werte vorhanden sind sind nutzen wir die Funktionsweise von "letzter Wert" am Tag, der dann auch den Wert von Vortagen ausgibt.
-            if ($this->ReadPropertyInteger('ValueType') == 0) {
-                $values = AC_GetLoggedValues($acID, $variableID, $date, $date + (24 * 3600) - 1, 0);
-                if ($values === false) {
-                    return;
-                }
-            }
-
-            //Der letzte Wert am Tag fragt alle Werte bis zum Endzeitpunkt ab mit Limit 1.
-            //Da AC_GetLoggedValues immer den neusten Wert zuerst ausgibt ist es genau der Wert den wir suchen
-            if ($this->ReadPropertyInteger('ValueType') == 1 || (isset($values) && count($values) == 0)) {
-                $values = AC_GetLoggedValues($acID, $variableID, 0, $date + (24 * 3600) - 1, 1);
-                if ($values === false) {
-                    return;
-                }
-            }
-
-            if (!isset($values) || count($values) == 0) {
-                echo 'Leider wurden für den gewählten Zeitraum keine Werte im Archiv gespeichert!';
-                return;
-            }
-
-            //Immer den letzten Wert im Array ausgeben
-            SetValue($this->GetIDForIdent('Reading'), $values[count($values) - 1]['Value']);
+        //Add references
+        foreach ($this->GetReferenceList() as $referenceID) {
+            $this->UnregisterReference($referenceID);
+        }
+        if (IPS_VariableExists($sourceVariable)) {
+            $this->RegisterReference($sourceVariable);
         }
     }
+
+    public function RequestAction($Ident, $Value)
+    {
+        switch ($Ident) {
+            case 'Date':
+                //Neuen Wert in die Statusvariable schreiben
+                SetValue($this->GetIDForIdent($Ident), $Value);
+
+                //Berechnen
+                $this->Calculate();
+                break;
+            default:
+                throw new Exception('Invalid Ident');
+        }
+    }
+
+    /**
+     * This function will be available automatically after the module is imported with the module control.
+     * Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+     *
+     * ZST_Calculate($id);
+     *
+     */
+    public function Calculate()
+    {
+        $acID = IPS_GetInstanceListByModuleID('{43192F0B-135B-4CE7-A0A7-1475603F3060}')[0];
+        $variableID = $this->ReadPropertyInteger('SourceVariable');
+        $date = strtotime(date('d-m-Y', GetValue($this->GetIDForIdent('Date'))) . ' 00:00:00');
+
+        //Falls der "erste Wert" am Tag gesucht wird, betrachte erstmal nur den gewählten Tag.
+        //Falls dort keine Werte vorhanden sind sind nutzen wir die Funktionsweise von "letzter Wert" am Tag, der dann auch den Wert von Vortagen ausgibt.
+        if ($this->ReadPropertyInteger('ValueType') == 0) {
+            $values = AC_GetLoggedValues($acID, $variableID, $date, $date + (24 * 3600) - 1, 0);
+            if ($values === false) {
+                return;
+            }
+        }
+
+        //Der letzte Wert am Tag fragt alle Werte bis zum Endzeitpunkt ab mit Limit 1.
+        //Da AC_GetLoggedValues immer den neusten Wert zuerst ausgibt ist es genau der Wert den wir suchen
+        if ($this->ReadPropertyInteger('ValueType') == 1 || (isset($values) && count($values) == 0)) {
+            $values = AC_GetLoggedValues($acID, $variableID, 0, $date + (24 * 3600) - 1, 1);
+            if ($values === false) {
+                return;
+            }
+        }
+
+        if (!isset($values) || count($values) == 0) {
+            echo 'Leider wurden für den gewählten Zeitraum keine Werte im Archiv gespeichert!';
+            return;
+        }
+
+        //Immer den letzten Wert im Array ausgeben
+        SetValue($this->GetIDForIdent('Reading'), $values[count($values) - 1]['Value']);
+    }
+}
